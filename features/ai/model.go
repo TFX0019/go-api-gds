9f9fc3f4bb602c1eaabed1b3986b095a0ddab577
unknown <1400kss@gmail.com>
2026-03-06 01:22:20 -0400

    feat: add response_text to output de ia generation

--- features/ai/model.go
+++ features/ai/model.go
@@ -8,14 +8,15 @@ import (
 )
 
 type AIGeneration struct {
-	ID          uuid.UUID `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
-	UserID      uint      `gorm:"not null" json:"user_id"`
-	User        auth.User `gorm:"foreignKey:UserID" json:"-"`
-	Prompt      string    `gorm:"type:text;not null" json:"prompt"`
-	ImageInput  *string   `gorm:"type:text" json:"image_input"`
-	ImageOutput *string   `gorm:"type:text" json:"image_output"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID           uuid.UUID `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	UserID       uint      `gorm:"not null" json:"user_id"`
+	User         auth.User `gorm:"foreignKey:UserID" json:"-"`
+	Prompt       string    `gorm:"type:text;not null" json:"prompt"`
+	ImageInput   *string   `gorm:"type:text" json:"image_input"`
+	ImageOutput  *string   `gorm:"type:text" json:"image_output"`
+	ResponseText *string   `gorm:"type:text" json:"response_text"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
 }
 
 func (AIGeneration) TableName() string {
